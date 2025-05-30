dbe3c04febb2e443d2a597acf2c85bb022b766d9
mercedesssr <208200631+mercedesssr@users.noreply.github.com>
2025-05-29 21:38:37 -0400

    java

---
+++ index.js
@@ -0,0 +1,14 @@
+let code1;
+code1 = 7+3
+console.log(code1);
+let code2 = 88 / 2 - 4 ;
+console.log (code2)
+let code3 = 139 % 100;
+console.log (code3);
+let message = "The vault has been secured. The combination is:";
+console.log(message);
+let codeA = code1 + "-" + code2 + "-" + code3;
+console.log(codeA)
+let codeB = `${code1}-${code2}-${code3}`
+console.log(codeB)
+console.log(message, codeA, codeB)
